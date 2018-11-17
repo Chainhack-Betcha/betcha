26156e4dac968a4fb9abf6c15b1420690fe05577
hailey <hyf16@ic.ac.uk>
2018-11-17 22:55:11 +0000

    Just the basic struct

--- client/src/App.js
+++ client/src/App.js
@@ -2,70 +2,83 @@ import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
+import MainPage from './mainPage';
+import Typography from '@material-ui/core/Typography';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 
 import "./App.css";
+
+const muiTheme = createMuiTheme({
+  palette: {
+    primary: {main : '#83af7c'},
+    secondary: {main: '#ffffff'},
+
+  },
+  fontFamily: 'Roboto, sans-serif',
+});
+
+const styles = {
+  root: {
+    height: '100%',
+  }
+}
 
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+  // componentDidMount = async () => {
+  //   try {
+  //     // Get network provider and web3 instance.
+  //     const web3 = await getWeb3();
 
-      // Get the contract instance.
-      const Contract = truffleContract(SimpleStorageContract);
-      Contract.setProvider(web3.currentProvider);
-      const instance = await Contract.deployed();
+  //     // Use web3 to get the user's accounts.
+  //     const accounts = await web3.eth.getAccounts();
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.log(error);
-    }
-  };
+  //     // Get the contract instance.
+  //     const Contract = truffleContract(SimpleStorageContract);
+  //     Contract.setProvider(web3.currentProvider);
+  //     const instance = await Contract.deployed();
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  //     // Set web3, accounts, and contract to the state, and then proceed with an
+  //     // example of interacting with the contract's methods.
+  //     this.setState({ web3, accounts, contract: instance }, this.runExample);
+  //   } catch (error) {
+  //     // Catch any errors for any of the above operations.
+  //     alert(
+  //       `Failed to load web3, accounts, or contract. Check console for details.`
+  //     );
+  //     console.log(error);
+  //   }
+  // };
 
-    // Stores a given value, 5 by default.
-    await contract.set(5, { from: accounts[0] });
+  // runExample = async () => {
+  //   const { accounts, contract } = this.state;
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.get();
+  //   // Stores a given value, 5 by default.
+  //   await contract.set(5, { from: accounts[0] });
 
-    // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
-  };
+  //   // Get the value from the contract to prove it worked.
+  //   const response = await contract.get();
+
+  //   // Update state with the result.
+  //   this.setState({ storageValue: response.toNumber() });
+  // };
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+    const { classes } = this.props;
+    // if (!this.state.web3) {
+    //   return <div>Loading Web3, accounts, and contract...</div>;
+    // } 
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 37</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div className={classes.root} >
+       <MuiThemeProvider theme={muiTheme}>
+          <MainPage/>
+        </MuiThemeProvider>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ client/src/mainPage.js
@@ -0,0 +1,59 @@
+import React from "react"; 
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import BetCard from './betCard'
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import Add from '@material-ui/icons/Add';
+
+
+
+
+class MainPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    const betCards = ['haha'];
+
+  this.state = {
+    betCards,
+  };
+}
+
+  handleClick = () => {
+
+  }
+
+  render() {
+    const { betCards } = this.state;
+		return (
+	 <div>
+       <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant='h4' color="secondary">
+            Betcha
+          </Typography>
+          </Toolbar>
+          </AppBar>
+          <div>
+            {betCards.map((tcr) => (
+              <BetCard />
+            ))}
+          </div>
+          <Button variant="fab" onClick={this.handleClick}>
+            <Add/>
+          </Button>
+
+      </div>
+		);
+  }
+};
+
+
+
+export default MainPage;
