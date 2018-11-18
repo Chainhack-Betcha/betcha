2883d9f331b3b361848e5650e3c3d934617ae305
Noel Lee <leekayau@gmail.com>
2018-11-18 12:38:27 +0000

    Added token balance in the header

--- client/src/api/TokenService.js
+++ client/src/api/TokenService.js
@@ -1,20 +1,47 @@
 import { getTokenContract } from './config';
 
 export default class TokenService {
-  constructor(web3) {
-    this.web3 = web3;
+  constructor() {
+    this.web3 = null;
     this.token = null;
   }
 
-  async init() {
+  async init(web3) {
+    this.web3 = web3;
     this.token = await getTokenContract(this.web3);
+    window.token = this;
   }
 
   async balanceOf(account) {
-    return this.token.balanceOf(account);
+    return new Promise((resolve, reject) => {
+      this.token.balanceOf(account, (error, tx) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(tx);
+      });
+    });
   }
 
   async currentAccountBalance() {
-    return this.token.currentAccountBalance();
+    return new Promise((resolve, reject) => {
+      this.token.currentAccountBalance((error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+      });
+    });
+  }
+
+  async distributeTokens(addresses, value) {
+    return new Promise((resolve, reject) => {
+      this.token.distributeToken(addresses, value, (error, tx) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(tx);
+      });
+    });
   }
 }
